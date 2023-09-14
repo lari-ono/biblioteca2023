54d58e38f6eab8c015690d2c1aa94ac9767636d2
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2023-09-14 23:52:23 +0000

    generoController pt.2

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -5,4 +5,28 @@ import org.sprinframework.stereotype.Controller;
 import org.sprinframework.ui.Model;
 import org.sprinframework.web.bind.annotation.RequestMapping;
 import org.sprinframework.web.bind.annotation.RequestMethod;
-import org.sprinframework.web.bind.annotation.RequestP
+import org.sprinframework.web.bind.annotation.RequestParam;
+
+import application.model.Genero;
+import applicatioin.model.GeneroRepository;
+
+@Controller
+@RequestMapping("/genero")
+public class GeneroController {
+    @Autowired
+    private GeneroRepository generoRepo;
+
+    @RequestMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("generos", generoRepo.findAll());
+        return "/genero/list";
+    }
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/genero/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public
+}

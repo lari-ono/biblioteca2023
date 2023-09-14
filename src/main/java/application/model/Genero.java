d4f832eff4fdc26f16a1a4f0f1a64c56b464f4ce
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2023-09-14 23:33:12 +0000

    termino genero

--- src/main/java/application/model/Genero.java
+++ src/main/java/application/model/Genero.java
@@ -7,4 +7,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

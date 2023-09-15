e5670421249a4e975af46b83dcf1a5dd9190a740
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2023-09-15 12:35:07 +0000

    correcao

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -1,11 +1,11 @@
 package application.controller;
 
-import org.sprinframework.beans.factory.annotation.Autowired;
-import org.sprinframework.stereotype.Controller;
-import org.sprinframework.ui.Model;
-import org.sprinframework.web.bind.annotation.RequestMapping;
-import org.sprinframework.web.bind.annotation.RequestMethod;
-import org.sprinframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Genero;
 import applicatioin.model.GeneroRepository;
